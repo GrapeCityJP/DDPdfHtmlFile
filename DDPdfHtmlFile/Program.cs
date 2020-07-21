b23f74409a65c9721f804d2c3edb79cf88a2a4f1
Kuni <kuni.ujiie@grapecity.com>
2020-07-21 13:31:41 +0900

    プロジェクト ファイルを追加します。

---
+++ DDPdfHtmlFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using GrapeCity.Documents.Html;
+using GrapeCity.Documents.Pdf;
+using System.Drawing;
+using System.IO;
+
+namespace DDPdfHtmlFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GcPdfDocument.SetLicenseKey("");
+
+            var doc = new GcPdfDocument();
+            var page = doc.NewPage();
+            var g = page.Graphics;
+
+            g.DrawHtml(File.ReadAllText("ActiveReports.html"),
+                       72,
+                       72,
+                       new HtmlToPdfFormat(false),
+                       out SizeF size);
+
+            doc.Save("HTMLFileToPDF.pdf");
+
+
+            var gcHtmlRenderer = new GcHtmlRenderer(File.ReadAllText("ActiveReports.html"));
+
+            gcHtmlRenderer.RenderToJpeg("HTMLFileToPDF.jpeg", new JpegSettings());
+            gcHtmlRenderer.RenderToPng("HTMLFileToPDF.png", new PngSettings());
+        }
+    }
+}
